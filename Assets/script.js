da47352ec9d92a28ff4e70e3960d05dfe2cd7532
Krimeas <krimeass@gmail.com>
2022-09-30 03:22:19 -0500

    Index & CSS left alone due to being supplied.  JS completed 90% fix final errors required.  READme cokmplete.

--- Assets/script.js
+++ Assets/script.js
@@ -1,14 +1,176 @@
+//need to generate four arrays
+//need to add numbers
+//need to add special characters
+//need to add capital letters
+//need to add lowercase letters (maybe combine with caps)
+
+//Need to call on four arrays at random for each character in the 8-128 password
+//need prompts for person to type length they want.
+//need prompts for exclusions (numbers, lowercase, etc.)
+//must select/type at least one type (make it so nomatter what upper or lower they type in prompt it comes out correct.)
+//prompts start when button pushed.
+//password written to page when complete.
+
+
+
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
+
+// The list of possible 'characters' for the password as well as the possible length.  
+
+// character arrays pulling ASCII due to issues wiht string variables being 'encrypted' in the generator.  
+//Special Characters
+let characters = [33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47,];
+// Numeric - 0,1,2,3,4,5,6,7,8,9
+let numeric = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57,];
+
+//Letters A-Z, and a-z.
+let caseUp = [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90,];
+let caseLo = [91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122,];
+
+// This foxes weird errors, by why?  ASCII encription?  
+// const UPPERCASE_CODES = arrayFromLowToHigh(65, 90);
+// const LOWERCASE_CODES = arrayFromLowToHigh(97, 122);
+// const NUMBER_CODES = arrayFromLowToHigh(48, 57);
+// const SYMBOL_CODES = arrayFromLowToHigh(33, 47)
+//   .concat(arrayFromLowToHigh(58, 64))
+//   .concat(arrayFromLowToHigh(91, 96))
+//   .concat(arrayFromLowToHigh(123, 126));
+// function arrayFromLowToHigh(low, high) {
+//   const array = [];
+//   for (let i = low; i <= high; i++) {
+//     array.push(i);
+//   }
+//   return array;
+// }
+
+
+// 
+// let charLength = [...Array(128).keys()]
+
+function generatePassword () {
+
+// Prompts user interaction and choices. characterlength gives alert is incorrect entry is made. 
+  let numericTF = confirm("Would you like numbers to appear in your password?");
+  let charactersTF = confirm("Would you like special characters to appear in your password?");
+  let caseLoTF = confirm("Would you like lowercase letters to appear in your password?");
+  let caseUpTF = confirm("Would you like uppercase letters to appear in your password?");
+
+  let characterLength = prompt("How many characters between 8 and 128 would you like?");
+    if (characterLength<8 || characterLength>128) {
+    alert("That is an invalid number.  Please enter a number between 8 and 128.");
+    }
+    
+  let PWFinal = [];
+
+//if Statement cycle for choosing which booleans to include
+
+//Same errors as below... must be variable issues.  
+  if (charactersTF) 
+  PWFinal = PWFinal.concat(characters)
+  if (numericTF) 
+  PWFinal = PWFinal.concat(numeric)
+  if (caseUpTF) 
+  PWFinal = PWFinal.concat(caseUp)
+  if (caseLoTF) 
+  PWFinal = PWFinal.concat(caseLo)
+
+  //Could not get away from errors on listed if's - changed strategy to above; maybe the character arrays?
+  // if (numericTF, charactersTF, caseLoTF, caseUpTF) {
+  //   PWFinal = PWFinal.concat(caseLo, caseUp, characters, numeric);
+  //     //TTTT - Works
+  // }
+  // if (numericTF && !charactersTF && caseLoTF && caseUpTF){
+  //   PWFinal = PWFinal.concat(numeric, caseLo, caseUp);
+  //     //TFTT - Errors
+  // }
+  // if (numericTF && charactersTF && !caseLoTF && caseUpTF){
+  //   PWFinal = PWFinal.concat(numeric, characters, caseUp);
+  //     //TTFT - Errors
+  // }
+  // if (numericTF && !charactersTF && !caseLoTF && caseUpTF){
+  //   PWFinal = PWFinal.concat(numeric,caseUp);
+  //     //TFFT - Errors
+  // }
+  // if (numericTF && charactersTF && caseLoTF && !caseUpTF){
+  //   PWFinal = PWFinal.concat(numeric,characters,caseLo);
+  //     //TTTF - Works
+  // }
+  // if (numericTF && !charactersTF && caseLoTF && !caseUpTF){
+  //   PWFinal = PWFinal.concat(numeric,caseLo);
+  //     //TFTF - Half Works - Characters still but no Uppercase.
+  // }
+  // if (numericTF && charactersTF && !caseLoTF && !caseUpTF){
+  //   PWFinal = PWFinal.concat(numeric,characters);
+  //     //TTFF - Works
+  // }
+  // if (numericTF && !charactersTF && !caseLoTF && !caseUpTF){
+  //   PWFinal = PWFinal.concat(numeric);
+  //     //TFFF - Works
+  // }
+  // if (!numericTF && charactersTF && caseLoTF && caseUpTF){
+  //   PWFinal = PWFinal.concat(characters,caseLo,caseUp);
+  //     //FTTT - Errors
+  // }
+  // if (!numericTF && !charactersTF && caseLoTF && caseUpTF){
+  //   PWFinal = PWFinal.concat(caseLo,caseUp);
+  //     //FFTT - Errors
+  // }
+  // if (!numericTF && charactersTF && !caseLoTF && caseUpTF){
+  //   PWFinal = PWFinal.concat(characters,caseUp);
+  //     //FTFT - Errors
+  // }
+  // if (!numericTF && !charactersTF && !caseLoTF && caseUpTF){
+  //   PWFinal = PWFinal.concat(caseUp);
+  //     //FFFT - Errors
+  // }
+  // if (!numericTF && charactersTF && caseLoTF && !caseUpTF){
+  //   PWFinal = PWFinal.concat(characters,caseLo);
+  //     //FTTF - Works
+  // }
+  // if (!numericTF && !charactersTF && caseLoTF && !caseUpTF){
+  //   PWFinal = PWFinal.concat(caseLo);
+  //     //FFTF - Half works (adds in characters for some reason?)
+  // }
+  // if (!numericTF && charactersTF && !caseLoTF && !caseUpTF){
+  //   PWFinal = PWFinal.concat(characters);
+  //     //FTFF - Works
+  // }
+
+//warning about no character types selected
+  if (!numericTF && !charactersTF && !caseLoTF && !caseUpTF){
+    alert("You did not ask for any character types.");
+      //FFFF - Works
+  }
+
+// warning about no numbers
+  if (PWFinal.length === 0){
+  alert("We cannot generate a password without any values.");
+  }
+
+//No Idea how this works, sadly. Looks like function buried inside of it, becasue of return.  it is doing the math to make sure the statement puts out the correct qty of characters.
+  let passwordCharacters = Array.from({length: characterLength}, () => {
+    let characterCode = PWFinal[Math.floor(Math.random() * PWFinal.length)];
+    return String.fromCharCode(characterCode);
+      })
+  return passwordCharacters.join('');
+}
+
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  let password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
-}
+  }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # password-generator
 Password Generator up to 128 characters.  
+
+
+## Third Challenge for Bootcamp
+Password Generator Completion: Complete random password generator with specifics laid out in the task, add character limits, numbers, letters, symbols.  Used to generate strong password for employee and safety of sensitive information.
+
+## Password Generator
+Password generator generats password within 8-128 characters in length or throws errors. When you click the generate password button a series of prompts will as you to answer ok/cancel to 
+
+https://krimeas.github.io/First-Portfolio/
+
+![Portfolio](/challenges/challenge-03/password-generator/Assets/Capture.JPG)
+
+
+------------------------------------------------------------
+
+### Task assigned:
+  1. Button press must set off prompts & alerts.
+  2. Prompts must pull & store data for later use in function to generate password.
+  3. Prompts/confirms - ask if numbers, letters upper & Lower, symbols, input character count 8-128.
+  4. Denial of all character types returns failure message.
+  5. After all prompts - make password display in central box.  
+
+
+### Technologies used in this project were:
+  1. HTML
+  2. CSS
+  3. VSCode
+  4. Git bash for windows
+  5. Chrome Web Broswer & Dev Tools
+
+------------------------------------------------------------
+
+#### Additional Sources:
+
+  1. https://stackoverflow.com/questions/66668274/javascript-password-generator-broken-code
+  2. https://stackoverflow.com/questions/69851109/js-password-generator-character-length-not-working
+  3. https://stackoverflow.com/questions/3746725/how-to-create-an-array-containing-1-n
+  4. https://oeis.org/wiki/ASCII#ASCII_printable_characters
+  5. https://stackoverflow.com/questions/2310452/why-does-my-encrypted-string-looks-like-consisting-of-only-question-marks
+  6. https://stackoverflow.com/questions/21468099/random-character-generator-to-be-stored-in-array
+  7. https://stackoverflow.com/questions/55764908/how-can-i-have-a-confirmation-dialog-with-title-that-returns-true-false-on-ok
+
+ 
+Task details, initial JavaScript, CSS, and HTML documents supplied by UT Austin Coding Bootcamp.
+
+This code follows the MIT License!
